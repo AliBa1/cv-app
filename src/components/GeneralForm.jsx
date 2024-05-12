b1b9ed2e6d6e9e6e9d04b0d677ac21e356e80205
Ali <89438079+AliBa1@users.noreply.github.com>
2024-05-11 20:29:52 -0700

    made form setup

--- src/components/GeneralForm.jsx
+++ src/components/GeneralForm.jsx
@@ -2,13 +2,39 @@ import { useState } from 'react'
 // import './GeneralForm.css'
 
 function GeneralForm() {
-//   const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
 
   return (
-    <>
-      <h3>General Information</h3>
-    </>
-  )
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      setFirstName(firstName);
+      setLastName();
+      setPhone();
+      setEmail();
+      console.log(firstName);
+    }}>
+      <label>First Name</label>
+      <input value={firstName} onChange={e => setFirstName(e.target.value)} required/>
+      <label>Last Name</label>
+      <input value={lastName} onChange={e => setLastName(e.target.value)} required/>
+      <label>Phone</label>
+      <input value={phone} onChange={e => setPhone(e.target.value)} required/>
+      <label>Email</label>
+      <input value={email} onChange={e => setEmail(e.target.value)} required/>
+
+      <button type="submit">Enter</button>
+    </form>
+
+  // <form action="#" method="get" class="new-list-form hide">
+  //   <label for="new_list">List Name</label>
+  //   <input type="text" name="new_list" id="new_list" required>
+
+  //   <button class="submit-list-btn" type="submit">Add List</button>
+  // </form>
+  );
 }
 
 export default GeneralForm
